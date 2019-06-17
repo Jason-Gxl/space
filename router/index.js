7b3a2eae3effaac34b51651699903447fd812388
Jason <gujunsheng@kedacom.com>
2019-06-17 14:16:52 +0800

    update

--- router/index.js
+++ router/index.js
@@ -10,19 +10,19 @@ function router(req, res) {
 		case /^\/?$/.test(url):
 			render("index", res);
 		break;
-		case /^(\/view)?\/admin\/?([^\?]*)?/.test(url):
+		case /^(?:\/view)?\/admin\/?([^\?\.]*)?/.test(url):
 			render("view/admin/" + (RegExp.$1 || "index"), res);
 		break;
-		case /^(\/view)?\/dashboard\/?([^\?]*)?/.test(url):
+		case /^(?:\/view)?\/dashboard\/?([^\?\.]*)?/.test(url):
 			render("view/dashboard/" + (RegExp.$1 || "index"), res);
 		break;
-		case /^(\/view)?\/map\/?([^\?]*)?/.test(url):
+		case /^(?:\/view)?\/map\/?([^\?\.]*)?/.test(url):
 			render("view/map/" + (RegExp.$1 || "index"), res);
 		break;
-		case /^(\/view)?\/webgl\/?([^\?]*)?/.test(url):
+		case /^(?:\/view)?\/webgl\/?([^\?\.]*)?/.test(url):
 			render("view/webgl/" + (RegExp.$1 || "index"), res);
 		break;
-		case /^(\/view)?\/ePad\/?([^\?]*)?/.test(url):
+		case /^(?:\/view)?\/ePad\/?([^\?\.]*)?/.test(url):
 			render("view/ePad/" + (RegExp.$1 || "index"), res);
 		break;
 		default:
