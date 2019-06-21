685324ef9fff99c111a25e4ffa6980823eed30ec
Jason <gujunsheng@kedacom.com>
2019-06-21 16:57:18 +0800

    update

--- router/index.js
+++ router/index.js
@@ -6,26 +6,12 @@ var render = require("../render");
 function router(req, res) {
 	var url = req.url;
 
-	switch(true) {
-		case /^\/?$/.test(url):
-			render("index", res);
-		break;
-		case /^(?:\/view)?\/admin\/?([^\?\.]*)?/.test(url):
-			render("view/admin/" + (RegExp.$1 || "index"), res);
-		break;
-		case /^(?:\/view)?\/dashboard\/?([^\?\.]*)?/.test(url):
-			render("view/dashboard/" + (RegExp.$1 || "index"), res);
-		break;
-		case /^(?:\/view)?\/map\/?([^\?\.]*)?/.test(url):
-			render("view/map/" + (RegExp.$1 || "index"), res);
-		break;
-		case /^(?:\/view)?\/webgl\/?([^\?\.]*)?/.test(url):
-			render("view/webgl/" + (RegExp.$1 || "index"), res);
-		break;
-		case /^(?:\/view)?\/ePad\/?([^\?\.]*)?/.test(url):
-			render("view/ePad/" + (RegExp.$1 || "index"), res);
-		break;
-		default:
+	if(/^\/?$/.test(url)) {
+		render("index", res);
+	} else {
+		if(/^(?:\/view)?(\/[\w$-]+)\/?([^\?\.]*)?/.test(url)) {
+			render("view" + RegExp.$1 + "/" + (RegExp.$2 || "index"), res);
+		}
 	}
 }
 
